d3458091c8d6b3e922a8d9af28474a0f516abff7
AaronCS25 <aaron.camacho@utec.edu.pe>
2025-11-22 16:01:49 -0500

    feat(service_locator): register Dio and ChatRepository in ServiceLocator

--- lib/config/service_locator/service_locator.dart
+++ lib/config/service_locator/service_locator.dart
@@ -1,6 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+import 'package:rimac_app/config/config.dart';
 import 'package:rimac_app/features/auth/auth.dart';
+import 'package:rimac_app/features/chat/chat.dart';
 import 'package:rimac_app/features/shared/shared.dart';
 
 class ServiceLocator {
@@ -22,6 +25,18 @@ class ServiceLocator {
         authRepository: get<AuthRepository>(),
         dictStorageService: get<DictStorageService>(),
       )..checkAuthStatus(),
+    );
+
+    _getIt.registerLazySingleton<Dio>(() {
+      final dio = Dio(BaseOptions(baseUrl: Environment.apiUrl));
+      dio.interceptors.add(
+        AuthInterceptor(dio: dio, authCubit: get<AuthCubit>()),
+      );
+      return dio;
+    });
+
+    _getIt.registerLazySingleton<ChatRepository>(
+      () => ChatRepositoryImpl(ChatDatasourceImpl(dio: get<Dio>())),
     );
   }
 }
