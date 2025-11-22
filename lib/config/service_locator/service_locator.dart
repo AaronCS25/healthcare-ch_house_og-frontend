f77ebf65373ee4b8f29f34451b6d65e02647c2de
AaronCS25 <aaron.camacho@utec.edu.pe>
2025-11-22 00:46:45 -0500

    feat: add service locator for dependency injection and initialization

---
+++ lib/config/service_locator/service_locator.dart
@@ -0,0 +1,27 @@
+import 'package:get_it/get_it.dart';
+
+import 'package:rimac_app/features/auth/auth.dart';
+import 'package:rimac_app/features/shared/shared.dart';
+
+class ServiceLocator {
+  static final _getIt = GetIt.instance;
+
+  static T get<T extends Object>() => _getIt.get<T>();
+
+  static Future<void> init() async {
+    _getIt.registerLazySingleton<DictStorageService>(
+      () => DictStorageServiceImpl(),
+    );
+
+    _getIt.registerLazySingleton<AuthRepository>(
+      () => AuthRepositoryImpl(AuthDatasourceImpl()),
+    );
+
+    _getIt.registerSingleton<AuthCubit>(
+      AuthCubit(
+        authRepository: get<AuthRepository>(),
+        dictStorageService: get<DictStorageService>(),
+      )..checkAuthStatus(),
+    );
+  }
+}
